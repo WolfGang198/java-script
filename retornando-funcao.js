559c6d2955e748919dfce694d4bf36eee53693f7
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2023-02-24 21:30:00 -0300

    Add files via upload

---
+++ retornando-funcao.js
@@ -0,0 +1,13 @@
+function maiorQue(){
+    let aleatorio = Math.random()*100;
+    aleatorio = aleatorio.toFixed(0);
+
+    let func = function (valor){
+        console.log(`${valor} é maior que ${aleatorio}?`);
+        return valor > aleatorio;
+    };
+
+    return func;
+}
+
+console.log(maiorQue()(50));

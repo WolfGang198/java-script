bddf9421d4acc769d58596e41de0eed0394fdc27
WolfGang <95577533+WolfGang198@users.noreply.github.com>
2023-07-31 13:07:58 -0300

    Add files via upload

---
+++ variaveis/script.js
@@ -0,0 +1,12 @@
+var mensagem = "Ola, mundo!";
+const mensagem2 = "Ola";//nao pode ser alterada
+let mensagem3 = "Ola, de novo";
+mensagem4 = "Ola, pela ultima vez";
+
+a = 1;
+b = "1";
+c = true;
+
+mensagem = "Novo valor";
+
+console.log(mensagem); 
